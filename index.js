74b5556707af996e54de1e4e88dfab22f2f673e8
pratham choudhary <prathamchoudhary27037@gmail.com>
2020-10-22 22:25:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1*6;
+randomNumber1 = Math.floor(randomNumber1)+1;
+console.log(randomNumber1);
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2*6;
+randomNumber2 = Math.floor(randomNumber2)+1;
+console.log(randomNumber2);
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png")
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png")
+
+if(randomNumber1 === randomNumber2){
+  document.querySelector("h1").innerHTML="🏳️DRAW!🏳️";
+}else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML="Player 1 Wins!🚩";
+}else{
+  document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+}
